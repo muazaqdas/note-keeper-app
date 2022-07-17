0602e3cc571314036b7c7bddae3ee645964f17b2
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-07-18 03:32:44 +0530

    frontend code completed

--- src/Components/NoteArea.js
+++ src/Components/NoteArea.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
 import { Zoom } from '@mui/material';
