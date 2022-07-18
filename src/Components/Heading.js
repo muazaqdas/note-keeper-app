7f58347877897814ab25e164c8a45f8e578810fe
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-07-19 03:06:15 +0530

    repo link added

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Button from '@mui/material/Button';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+import { Grid } from "@mui/material";
+// import { BsGithub } from "react-icons/bs";
+import { CgGitFork } from "react-icons/cg";
+import { AiFillStar } from "react-icons/ai";
+
+
+import "../assets/style/heading.css"
 
 function Heading(){
     return(
         <header>
-            <h1 className="heading">Keeper</h1>
+        <Grid container spacing={1}>
+            <Grid item xs={9}>
+                <h1 className="heading">Keeper</h1>
+            </Grid>
+            <Grid item xs={3}>
+                <Button className="githubButton" href="https://github.com/muazaqdas/note-keeper-app" variant="outlined" target="_blank">
+                    <CgGitFork style={{ fontSize: "1.7em" }} />{" "}
+                    <AiFillStar style={{ fontSize: "1.6em" }} />
+                </Button>
+            </Grid>
+        </Grid>
         </header>
     
     )   
